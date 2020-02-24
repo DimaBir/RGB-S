67d34e484b2631e4f994b55719daf2b799e2f798
Dima Birenbaum <dvlasenko86@gmail.com>
2020-02-24 15:51:58 +0200

    Added CNN Base Model

---
+++ models/__init__.py
@@ -0,0 +1,32 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # 128x128
+        self.conv1 = nn.Conv2d(10, 32, kernel_size=3, stride=2, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        # 32x32
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1)
+        # 8x8
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1)
+        # 2x2
+        self.fc1 = nn.Linear(128 * 2 * 2, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 2)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        # Flatten
+        x = x.view(-1, 128 * 2 * 2)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
