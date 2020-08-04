66539f465b3818792fa51501c08c6715c7437a7b
Dima Birenbaum <dvlasenko86@gmail.com>
2020-08-04 11:32:46 +0300

    Added Dropout

--- models/__init__.py
+++ models/__init__.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self, num_of_channels: int = 10):
         super(Net, self).__init__()
+        # CONV + MAXPOOL
         # 128x128
         self.conv1 = nn.Conv2d(num_of_channels, 64, kernel_size=3, stride=2, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
@@ -14,17 +15,26 @@ class Net(nn.Module):
         # 8x8
         self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)
         # 2x2
+
+        # Dropouts
+        self.dropout1 = nn.Dropout2d(0.2)
+        self.dropout2 = nn.Dropout2d(0.5)
+
+        # FC
         self.fc1 = nn.Linear(256 * 2 * 2, 1024)
         self.fc2 = nn.Linear(1024, 512)
         self.fc3 = nn.Linear(512, 2)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
+        x = self.dropout1(x)
         x = self.pool(F.relu(self.conv2(x)))
+        x = self.dropout1(x)
         x = self.pool(F.relu(self.conv3(x)))
         # Flatten
         x = x.view(-1, 256 * 2 * 2)
         x = F.relu(self.fc1(x))
+        x = self.dropout2(x)
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
